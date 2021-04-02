947d6bf27a3f66db79eee7e1fe2809b29a904f21
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2021-04-02 15:33:02 +0530

    B4 complete

---
+++ B/B4/B4.js
@@ -0,0 +1,54 @@
+
+const listItems = document.querySelectorAll('a');
+
+
+function handleHover(){
+
+    this.classList.add("active");
+    
+    console.log(this.nextElementSibling.getBoundingClientRect); 
+
+    var dimensions ={
+
+        width: `${this.nextElementSibling.offsetWidth + 20}px`,
+        height: `${this.nextElementSibling.offsetHeight + 10}px`,
+        x_Cord: (this.nextElementSibling.getBoundingClientRect().left -10),
+        y_Cord: (document.querySelector('.dynamicContent').getBoundingClientRect().top -5)
+
+    }
+
+    console.log(dimensions.x_Cord);
+    console.log(dimensions.y_Cord);
+
+    document.querySelector('.dynamicBackground').style.width = dimensions.width;
+    document.querySelector('.dynamicBackground').style.height = dimensions.height;
+    document.querySelector('.dynamicBackground').style.transform = `translate(${dimensions.x_Cord}px,${dimensions.y_Cord}px)`
+}
+
+function handleEmpty(){
+
+    this.classList.remove("active");
+    document.querySelector('.dynamicBackground').style.width = "0px";
+    document.querySelector('.dynamicBackground').style.height = "0px";
+
+}
+
+
+
+// const contentDiv = document.querySelectorAll;
+
+// contentDiv.forEach(function(item){
+//     item.addEventListener('mouseEnter',function(){
+//          this.style.opacity = "1";
+//     })
+// })
+
+
+listItems[0].addEventListener('mouseenter', handleHover)
+listItems[1].addEventListener('mouseenter', handleHover)
+listItems[11].addEventListener('mouseenter', handleHover)
+
+
+listItems[0].addEventListener('mouseleave', handleEmpty)
+listItems[1].addEventListener('mouseleave', handleEmpty)
+listItems[11].addEventListener('mouseleave', handleEmpty)
