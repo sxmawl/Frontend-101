471b94896b26f1b370650c3e52093b4ed2efde68
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2021-04-08 21:39:53 +0530

    UI Changes

--- B/B1 JS/B1.js
+++ B/B1 JS/B1.js
@@ -5,16 +5,14 @@ const hoursHand = document.querySelector('.hours');
 
 setInterval(
     function(){
+        
 let today = new Date();
 let seconds = today.getSeconds();
 let hours = today.getHours()<=12 ? today.getHours() : today.getHours()-12 ;
 let minutes = today.getMinutes();
 
 
-secondsHand.style.transform = 'rotate(' + seconds*6 +'deg)';
-  
- console.log(secondsHand.style.transform);
-
+ secondsHand.style.transform = 'rotate(' + seconds*6 +'deg)';
     
  minutesHand.style.transform = 'rotate(' + (minutes + seconds/60)*6 +'deg)';
 
